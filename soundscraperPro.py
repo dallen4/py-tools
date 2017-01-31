652611f7f9c2c80890a3559547f24768f6a02188
Nieky Allen <dallen4@luc.edu>
2017-01-31 13:30:51 -0600

    update pip. progress on soundscraperPro.py

--- soundscraperPro.py
+++ soundscraperPro.py
@@ -3,14 +3,18 @@ import soundcloud
 import soundscrape
 from soundscrape.soundscrape import process_soundcloud
 
-#Look up tracks of Artist Example
 client = soundcloud.Client(client_id='175c043157ffae2c6d5fed16c3d95a4c')
-userTracks = client.get('/users/'+USERNAME+'/tracks')
-for track in userTracks:
-    trackURI = track.uri
-    track_info = client.get(uri)
-    trackTitle = track_info.title
-    print trackTitle
+
+#print("Welcome to SoundScraper Pro. \nBuilt on Miserlou's SoundScrape at https://github.com/Miserlou/SoundScrape. \nBy dallen4 @ https://github.com/dallen4.")
+#USERNAME = raw_input("What is the username of the artist you are looking for (as appears in the URL on their page)?\n")
+
+
+# userTracks = client.get('/users/brianfresco/tracks')
+# for track in userTracks:
+#     trackURI = track.uri
+#     track_info = client.get(trackURI)
+#     trackTitle = track_info.title
+#     print (trackTitle)
 
 #Download track from SoundCloud Example
 vargs = {'path':'', 'folders': False, 'group': False, 'track': 'miracle-prod-mano', 'num_tracks': 1, 'bandcamp': False, 'downloadable': False, 'likes': False, 'open': False, 'artist_url': 'brianfresco', 'keep': False}
